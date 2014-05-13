9db6959a3fa898982232b63e47ea17eacb4de1b7
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2014-05-12 19:28:35 -0500

    Include home folder, remove first argument
    The first argument is unceccesary since we'll always be backing up the entire
    system.
    
    The home folder wasn't being included before since it was on a separate drive.
    This forces it to be included regardless of where its located.

--- backup.sh
+++ backup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 RDIFF=rdiff-backup
 
-# first two arguments are source and destintation
-SOURCE=${1}
-DEST=${2}
+# always backup the root system
+SOURCE='/'
+
+# only argument is the destination
+DEST=${1}
 
 # configuration options
 VERBOSITY=5
 KEEP_FOR=3M
 
 echo "Backing up '${SOURCE}' to '${DEST}'..."
+echo
+
 ${RDIFF} \
+  --exclude '/home/*/.cache' \
+  --include '/home' \
   --exclude '/tmp/*' \
   --exclude '/proc/*' \
   --exclude '/sys/*' \
@@ -20,7 +26,10 @@ ${RDIFF} \
   --verbosity "${VERBOSITY}" \
   "${SOURCE}" "${DEST}"
 
+echo
 echo "Purging old backups in '${DEST}'..."
+echo
+
 ${RDIFF} \
   --remove-older-than "${KEEP_FOR}" \
   --force \
