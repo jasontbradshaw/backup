d9e89fd47b71aab08a76ef5ae2f8ea4dcb6c6a6a
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2014-05-31 19:01:58 -0500

    Add built-in locking

--- backup.sh
+++ backup.sh
@@ -1,15 +1,37 @@
 #!/usr/bin/env bash
 RDIFF=rdiff-backup
 
-# always backup the root system
-SOURCE='/'
-
-# only argument is the destination
-DEST=${1}
+# our only argument is the destination
+DEST="${1}"
 
 # configuration options
+SOURCE='/'
 VERBOSITY=5
 KEEP_FOR=3M
+
+# the file used to notify that a system backup is happening
+lockdir="/tmp/backup.lock"
+
+# information files for the current backup process
+pid_file="${lockdir}/pid"
+start_date_file="${lockdir}/start-date"
+
+# exit if we can't grab our lock
+if ! mkdir "${lockdir}" &> /dev/null; then
+  echo "Backup already in progress:"
+  echo "  Started: $(cat ${start_date_file})"
+  echo "      PID: $(cat ${pid_file})"
+  echo "Exiting."
+  exit 1
+fi
+
+# store useful info about this backup in the the lock directory
+echo "$BASHPID" > "${pid_file}"
+echo "$(date)" > "${start_date_file}"
+
+# always remove the lock directory we're about to create on exit, no matter
+# what. however, make sure we only do this once we know we have the lock!
+trap "rm -rf ${lockdir}" INT QUIT TERM EXIT
 
 echo "Backing up '${SOURCE}' to '${DEST}'..."
 echo
@@ -36,3 +58,6 @@ ${RDIFF} \
   --force \
   --verbosity "${VERBOSITY}" \
   "${DEST}"
+
+echo
+echo 'Backup complete!'
